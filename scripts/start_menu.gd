d296625fc2d9ffc16b36fc29169fa3757c5895dd
turdle <shuruiguo1@gmail.com>
2026-01-15 10:03:34 +1100

    start menu
    - hover over the cats for sound effects
    - click button in bottom right to continue!

---
+++ scripts/start_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_button_pressed() -> void: #start game button
+	click.play()
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+
+
+func _on_area_2d_mouse_entered() -> void: #cat meow when hovering!
+	var random_float = randf()
+	randomize()
+	if random_float <= 0.32:
+		meow.play()
+	if random_float >= 0.33 and random_float <= 0.66:
+		meow1.play()
+	if random_float >= 0.67:
+		meow2.play()
