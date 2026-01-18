289a0002afd8e7059b2cfdab37d9ec309145f468
turdle <shuruiguo1@gmail.com>
2026-01-18 12:49:18 +1100

    blobcats eating food
    - consumes 2500 feed per blobcat every 10 secs
    - text notification in top left
    - munching sound effects :3

--- scenes/main_scene.gd
+++ scenes/main_scene.gd
@@ -24,10 +24,17 @@ extends Node2D
 @onready var frenzy_sprite2 = $frenzy1
 @onready var frenzy_timer = $frenzy_timer
 @onready var frenzy_sound = $frenzy_sound
+@onready var blobcat_amount = 0
+@onready var started_feeding := false
+@onready var feeding_blobcat_timer = $feeding_blobcat
+@onready var munch_sound = $munch
+@onready var eating_label = $eating_label
+@onready var eating_animation = $eating
 func _ready():
 	feeding_text.hide()
 	frenzy_sprite.hide()
 	frenzy_sprite2.hide()
+	eating_label.hide()
 func _on_button_mouse_entered() -> void: #show price on hover
 	button.text = 'COST: 10  feed'
 func _on_button_mouse_exited() -> void:  #remove price after hover
@@ -54,6 +61,9 @@ func _process(delta: float) -> void:
 			catfeed_animation.play("catfeed")
 			click.play()
 			chance_for_frenzy()
+	if blobcat_amount >= 1 and not started_feeding:
+		started_feeding = true
+		feeding_blobcat_timer.start()
 
 
 
@@ -77,6 +87,7 @@ func _on_button_3_pressed() -> void: #blob cat (purchase)
 		tutorial.play("blobcat_feeding")
 		random_meow()
 		blobcat1_bought = true
+		blobcat_amount += 1
 
 func random_meow():
 	var random_float = randf()
@@ -124,12 +135,13 @@ func _on_button_4_mouse_entered() -> void:
 		button4.text = "BOUGHT"
 func _on_button_4_mouse_exited() -> void:
 	button4.text = "Blob cat 2"
-func _on_button_4_pressed() -> void:
+func _on_button_4_pressed() -> void:          #blobcat purchase
 	if clicky >= 9999 and not blobcat2_bought:
 		clicky -= 9999
 		blobcat2_animation.play("blobcat2_entry")
 		random_meow()
 		blobcat2_bought = true
+		blobcat_amount += 1
 
 
 func _on_blobcat_2_animation_animation_finished(anim_name: StringName) -> void:
@@ -156,3 +168,10 @@ func _on_autoclick_timer_timeout() -> void:
 
 func _on_frenzy_timer_timeout() -> void:
 	frenzy_multiplier = 1
+
+
+func _on_feeding_blobcat_timeout() -> void:
+	clicky -= 2500 * blobcat_amount
+	munch_sound.play()
+	eating_animation.play("eating")
+	eating_label.show()
